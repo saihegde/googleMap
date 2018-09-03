76e2b6677cc7a92e9bb0457c3586e7c2841e7fc3
Sai <saihegde@gmail.com>
2018-09-03 12:44:18 -0600

    Initial Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import { GoogleMaps, GoogleMap } from '@ionic-native/google-maps';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: GoogleMap;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ngAfterViewInit(){
+   this.initMap();
+  }
+
+  initMap() {
+    let element = this.mapElement.nativeElement;
+    this.map = GoogleMaps.create(element);
   }
 
 }
